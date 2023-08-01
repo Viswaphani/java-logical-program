2adcf320305815921300e1e6396eb5052e649536
Viswaphani <136645551+Viswaphani@users.noreply.github.com>
2023-08-01 17:51:11 -0500

    Create maxnum.java

---
+++ maxnum.java
@@ -0,0 +1,23 @@
+/*Write a JAVAprogram to find maximum between two numbers*/
+import java.util.*;
+public maxnum
+{
+
+public static void main(String[]args)
+{
+ Scanner sc=new Scanner(System.in);
+ int  a,b;
+ System.out.print("Enter a value ");
+ a=sc.nextInt();
+ System.out.print("Enter b value ");
+ b=sc.nextInt();
+ if(a>b)
+ {
+ System.out.println("a is maximum");
+ }
+ else
+ {
+  System.out.println("b is maximum");  
+ }
+}
+}
