56835ec6093bcc2a6d49682625236a659271167d
Viswaphani <136645551+Viswaphani@users.noreply.github.com>
2023-08-03 19:45:50 -0500

    Create checkalphabet.java

---
+++ checkalphabet.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class checkalphabet{
+public static void main(String[] args) {
+Scanner scanner = new Scanner(System.in);
+
+System.out.print("Enter an alphabet");
+char ch = scanner.next().charAt(0);
+
+if (isVowel(ch))
+{
+System.out.println(ch + " is a vowel");
+}
+else
+{
+System.out.println(ch + " is a consonant");
+}
+}
+
+ public static boolean isVowel(char ch)
+ {
+ch = Character.toLowerCase(ch);
+
+
+return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
+    }
+}
