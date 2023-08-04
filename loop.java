077bd2e4010b48c80dbb18379793722a45a86c57
Viswaphani <136645551+Viswaphani@users.noreply.github.com>
2023-08-03 21:58:23 -0500

    Create loop.java

---
+++ loop.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class loop
+{
+
+public static void main(String[]args)
+{
+ Scanner sc=new Scanner(System.in);
+
+int i,n;
+n=sc.nextInt();
+for(i=1;i<=n;i++)
+
+{
+System.out.println(i);
+}
+}
+}
