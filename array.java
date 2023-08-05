ed8769c3bfe595dd66c7de9846a5f0abc7864e43
Viswaphani <136645551+Viswaphani@users.noreply.github.com>
2023-08-04 21:39:01 -0500

    Create array.java

---
+++ array.java
@@ -0,0 +1,13 @@
+public class array
+{
+	public static void main(String[] args) {
+	int a[]={10,20,30,40,50};
+	int i;
+	System.out.println("value of array");
+	for( i=0;i<a.length;i++)
+	{
+	    System.out.println(a[i]);
+	}
+	
+	}
+}
