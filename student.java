441a64f39e72af68b17afbf0707428d6b383d761
Viswaphani <136645551+Viswaphani@users.noreply.github.com>
2023-08-15 19:11:55 -0500

    Create student.java

---
+++ student.java
@@ -0,0 +1,36 @@
+import java.util.*;
+class student
+{
+ int id;
+ String sname;
+ String email;
+ Scanner sc=new Scanner(System.in);
+
+
+ 
+void getstudent()
+{
+System.out.println("enter student name:");
+sname=sc.next();
+System.out.println("enter student id:");
+id=sc.nextInt();
+System.out.println("enter student email:");
+email=sc.next();
+}
+void dispstudent()
+{
+System.out.println("student name:"+sname);
+System.out.println(" student id:"+id);
+System.out.println("student email:"+email);
+}
+}
+class Main
+{
+    public static void main (String[] args) 
+    {
+      student s=new student();
+     
+ s.getstudent();
+ s.dispstudent();
+    }
+}
